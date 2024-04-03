8271ab80f0950cfcea7d782a2198111aa5f53b5e
SihamELBOUBAKRI <sihamelboubakri8@gmail.com>
2024-04-03 01:38:17 +0000

    all

---
+++ candy.js
@@ -0,0 +1,217 @@
+var candies = ["Blue", "Orange", "Green", "Yellow", "Red", "Purple"];
+var board = [];
+var rows = 9;
+var columns = 9;
+var score = 0;
+var time = 30; // Initial time
+var timerInterval; 
+
+window.onload = function() {
+    startGame();
+
+    // Start the timer
+    timerInterval = setInterval(updateTime, 1000);
+
+    // bach ndir Check game over kola tanya 
+    window.setInterval(function(){
+        crushCandy();
+        slideCandy();
+        generateCandy();
+        if (time == 0) {
+            gameOver();
+        }
+    }, 1000);
+}
+
+function updateTime() {
+    time--; //kn9es btanya
+    document.getElementById("timer").innerText = time; 
+}
+
+function randomCandy() {
+    return candies[Math.floor(Math.random() * candies.length)]; //0 - 5.99
+}
+
+function startGame() {
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            let tile = document.createElement("img");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.src = "./images/" + randomCandy() + ".png";
+
+            tile.addEventListener("dragstart", dragStart);
+            tile.addEventListener("dragover", dragOver);
+            tile.addEventListener("dragenter", dragEnter);
+            tile.addEventListener("dragleave", dragLeave);
+            tile.addEventListener("drop", dragDrop);
+            tile.addEventListener("dragend", dragEnd);
+
+            document.getElementById("board").append(tile);
+            row.push(tile);
+        }
+        board.push(row);
+    }
+
+    console.log(board);
+}
+
+function dragStart() {
+    currTile = this;
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+}
+
+function dragDrop() {
+    otherTile = this;
+}
+
+function dragEnd() {
+
+    if (currTile.src.includes("blank") || otherTile.src.includes("blank")) {
+        return;
+    }
+
+    let currCoords = currTile.id.split("-");
+    let r = parseInt(currCoords[0]);
+    let c = parseInt(currCoords[1]);
+
+    let otherCoords = otherTile.id.split("-");
+    let r2 = parseInt(otherCoords[0]);
+    let c2 = parseInt(otherCoords[1]);
+
+    let moveLeft = c2 == c-1 && r == r2;
+    let moveRight = c2 == c+1 && r == r2;
+
+    let moveUp = r2 == r-1 && c == c2;
+    let moveDown = r2 == r+1 && c == c2;
+
+    let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+
+    if (isAdjacent) {
+        let currImg = currTile.src;
+        let otherImg = otherTile.src;
+        currTile.src = otherImg;
+        otherTile.src = currImg;
+
+        let validMove = checkValid();
+        if (!validMove) {// ye3ni checkvalid falsa oky3awdo it9lbo lya candies okyrej3o kikano
+            let currImg = currTile.src;
+            let otherImg = otherTile.src;
+            currTile.src = otherImg;
+            otherTile.src = currImg;    
+        }
+        else {
+            // Ifla kant valide move zid mn we9t b 3 seconds
+            time += 3;
+            document.getElementById("timer").innerText = time;
+        }
+    }
+}
+
+function crushCandy() {
+    crushThree();
+    document.getElementById("score").innerText = score;
+}
+
+function crushThree() {
+    // Check rows
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns-2; c++) {
+            let candy1 = board[r][c];
+            let candy2 = board[r][c+1];
+            let candy3 = board[r][c+2];
+            if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
+                candy1.src = "./images/blank.png";
+                candy2.src = "./images/blank.png";
+                candy3.src = "./images/blank.png";
+                score += 10;
+                time++; 
+            }
+        }
+    }
+
+    // Check columns
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows-2; r++) {
+            let candy1 = board[r][c];
+            let candy2 = board[r+1][c];
+            let candy3 = board[r+2][c];
+            if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
+                candy1.src = "./images/blank.png";
+                candy2.src = "./images/blank.png";
+                candy3.src = "./images/blank.png";
+                score += 10;
+                time++; 
+            }
+        }
+    }
+}
+
+function checkValid() {
+    // Check rows
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns-2; c++) {
+            let candy1 = board[r][c];
+            let candy2 = board[r][c+1];
+            let candy3 = board[r][c+2];
+            if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
+                return true;
+            }
+        }
+    }
+
+    // Check columns
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows-2; r++) {
+            let candy1 = board[r][c];
+            let candy2 = board[r+1][c];
+            let candy3 = board[r+2][c];
+            if (candy1.src == candy2.src && candy2.src == candy3.src && !candy1.src.includes("blank")) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+function slideCandy() {
+    for (let c = 0; c < columns; c++) {
+        let ind = rows - 1;
+        for (let r = columns-1; r >= 0; r--) {
+            if (!board[r][c].src.includes("blank")) {
+                board[ind][c].src = board[r][c].src;
+                ind -= 1;
+            }
+        }
+
+        for (let r = ind; r >= 0; r--) {
+            board[r][c].src = "./images/blank.png";
+        }
+    }
+}
+
+function generateCandy() {
+    for (let c = 0; c < columns;  c++) {
+        if (board[0][c].src.includes("blank")) {
+            board[0][c].src = "./images/" + randomCandy() + ".png";
+        }
+    }
+}
+
+function gameOver() {
+    clearInterval(timerInterval); 
+    document.getElementById("gameOverMessage").innerText = "Game Over!"; 
+    document.getElementById("gameOverMessage").style.display = "block"; 
+    document.getElementById("refreshButton").style.display = "block";
+}
